7c9ff2c1a470ed644a41a898c14a1c53c54bc42a
Gunnthorian <gunnarsbjorkman@gmail.com>
2016-10-06 18:30:13 -0400

    mobile support navigation
    added an incredible navigation for smaller devices

--- js/sheet.js
+++ js/sheet.js
@@ -1,13 +1,20 @@
 function screenSizeGrab(){
   var width = window.innerWidth;
-
   var height = window.innerHeight;
-
   document.getElementById('homeimg').style.width = width + "px";
+}
 
-    var w = window.outerWidth;
-      var h = window.outerHeight;
-      var txt = "Window size: width=" + w + ", height=" + h;
-      document.getElementById("demo").innerHTML = txt;
+function mobileNav(){
 
+  if(document.getElementById("di").value!="ON"){
+    document.getElementById("di").value="ON";
+    document.getElementById("di").style.display = "block";
+    document.getElementById("dii").className = "dropitemsimgup";
+    document.getElementById("di").className = "dropped-div-transition-down";
+ }else{
+    document.getElementById("di").value="OFF";
+    document.getElementById("di").style.display = "none";
+    document.getElementById("dii").className = "dropitemsimgdown";
+    document.getElementById("di").className = "dropped-div-transition-up";
+ }
 }
