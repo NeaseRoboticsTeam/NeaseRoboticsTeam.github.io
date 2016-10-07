ad850760b003711bf0bae15b902a4a0dcbea6214
Gunnthorian <gunnarsbjorkman@gmail.com>
2016-10-06 20:29:28 -0400

    why does merging mess everything up?

--- js/sheet.js
+++ js/sheet.js
@@ -1,6 +1,5 @@
 function screenSizeGrab(){
   var width = window.innerWidth;
-<<<<<<< Updated upstream
   var height = window.innerHeight;
   document.getElementById('homeimg').style.width = width + "px";
 }
@@ -19,16 +18,3 @@ function mobileNav(){
     document.getElementById("di").className = "dropped-div-transition-up";
  }
 }
-=======
-
-  var height = window.innerHeight;
-
-  document.getElementById('homeimg').style.width = width + "px";
-
-    var w = window.outerWidth;
-      var h = window.outerHeight;
-      var txt = "Window size: width=" + w + ", height=" + h;
-      document.getElementById("demo").innerHTML = txt;
-
-}
->>>>>>> Stashed changes
